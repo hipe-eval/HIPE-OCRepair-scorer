fe4f0f64673d781ea9b161fef208ceba937a90b0
Simon Clematide <simon.clematide@uzh.ch>
2026-04-11 03:40:29 +0200

    bump version to 0.9.6

--- hipe_ocrepair_scorer/__init__.py
+++ hipe_ocrepair_scorer/__init__.py
@@ -1,5 +1,5 @@
 from .ocrepair_eval import Evaluation, mer_from_counts, align_records, norm
 from .cli import main as cli_entry
 
-__version__ = "0.9.5"
+__version__ = "0.9.6"
 __all__ = ["Evaluation", "align_records","norm"]
